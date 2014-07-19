76ec4f1b1f77093ea94e52e3d206f06a3358a160
willnet <netwillnet@gmail.com>
2014-07-19 11:02:59 +0900

    Delete working model

--- app/models/working.rb
+++
@@ -1,4 +0,0 @@
-class Working < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :assignment
-end
--- spec/factories/workings.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :working do
-    user nil
-    assignment nil
-  end
-end
