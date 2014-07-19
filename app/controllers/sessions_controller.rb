22d3c524f52e5d3a37d4b7b133523fe143eb2c09
willnet <netwillnet@gmail.com>
2014-07-19 18:28:16 +0900

    Delete flash message when login, logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to assignments_path, notice: 'ログインしました'
+    redirect_to assignments_path
   end
 
   def destroy
     reset_session
-    redirect_to root_path, notice: 'ログアウトしました'
+    redirect_to root_path
   end
 
   def failure
-    redirect_to root_url, alert: "ログインに失敗しました"
+    redirect_to root_url
   end
 end
